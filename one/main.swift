d1ca3db37667a5e67932eb80b91df3d43680f697
Анатолий Богдюн <bobototo17@gmail.com>
2022-04-04 19:34:07 +0300

    Вторая домашняя работа выполнена

--- one/main.swift
+++ one/main.swift
@@ -7,5 +7,49 @@
 
 import Foundation
 
-print("Hello, World!")
+//1. Написать функцию, которая определяет, четное число или нет.
+
+func evenOrOdd(_ number: Int) {
+    if number % 2 == 0 {
+        print("Число четное")
+    } else {
+        print("Число нечетное")
+    }
+}
+
+evenOrOdd(0)
+evenOrOdd(9)
+
+print("=======")
+
+//2.Написать функцию, которая определяет, делится ли число без остатка на 3.
+
+func divisionWithoutRemainderByThree(number: Int) {
+    number % 3 == 0 ? print("Делится без остатка на 3") : print("Не делится без остатка на 3")
+}
+
+divisionWithoutRemainderByThree(number: 10)
+divisionWithoutRemainderByThree(number: 18)
+
+print("=========")
+
+//3. Создать возрастающий массив из 100 чисел.
+
+var growingArray = [Int]()
+
+for i in 1...100 {
+    growingArray.append(i)
+}
+print(growingArray)
+
+print("=========")
+
+//4. Удалить из этого массива все четные числа и все числа, которые не делятся на 3.
+
+for (_, value) in growingArray.enumerated() {
+    if value % 2 == 0 || value % 3 > 0 {
+        growingArray.remove(at: growingArray.firstIndex(of: value)!)
+    }
+}
+print(growingArray)
 
